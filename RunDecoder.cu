592c5127070f89601801c2a2d4522185050e6e77
komp <komp@ittc.ku.edu>
2017-09-20 14:52:20 -0500

    Now use ldpcEncoder instead of reading data from file
    for received signal.

--- RunDecoder.cu
+++ RunDecoder.cu
@@ -1,3 +1,4 @@
+#include <random>
 #include <math.h>
 #include <string.h>
 #include <stdio.h>
@@ -11,6 +12,11 @@
 
 #define MAXITERATIONS  200
 
+void ldpcEncoder (unsigned int *infoWord, unsigned int* W_ROW_ROM,
+                  unsigned int numMsgBits, unsigned int numRowsinRom, unsigned int numParBits,
+                   unsigned int shiftRegLength,
+                  unsigned int *codeWord);
+
 int ldpcDecoder (float *rSig, unsigned int numChecks, unsigned int numBits,
                  unsigned int maxBitsForCheck, unsigned int maxChecksForBit,
                  unsigned int *mapRows2Cols, unsigned int *mapCols2Rows,
@@ -22,26 +28,53 @@ int main (int argc, char **argv) {
 
   unsigned int numChecks, numBits, maxBitsForCheck, maxChecksForBit;
   unsigned int  *mapRows2Cols, *mapCols2Rows;
-  float *receivedSigs;
-  unsigned int sigLength, numSigs;
-  char mapFile[256];        /* = "./SampleData/Maps1024_3.0.bin"; */
-  char* sigFile;            /* = "./SampleData/sig_3.0.bin"; */
+  unsigned int numRowsW, numColsW, numParityBits, shiftRegLength;
+  unsigned int *W_ROW_ROM;
+
+  char mapFile[256];
+  char wROM_File[256];
   FILE *src;
   int errnum;
-  unsigned int infoleng, rnum, rdenom;
+  unsigned int infoLeng, rnum, rdenom;
   float ebno;
+  unsigned int how_many;
+  float rNominal;
+  float No, sigma2, lc;
+
+  unsigned int  seed = 163331;
+  /*  or use this to get a fresh sequence each time the program is run.
+  std::random_device  rd;  //Will be used to obtain a seed for the random number engine
+  std::mt19937 generator(rd()); //Standard mersenne_twister_engine seeded with rd()
+  */
+  std::mt19937 generator(seed); //Standard mersenne_twister_engine
+  std::uniform_real_distribution<> rDist(0, 1);
+
+  // normal distribution for noise.
+  // NOTE,  we are using the same random number generator ("generator") here.
+  std::normal_distribution<float> normDist(0.0, 1.0);
+
 
   if (argc < 6) {
-    printf("usage:  RunDecoder <infolength> <r-numerator> <r-denominator> <ebno> <signal-filename>\n" );
+    printf("usage:  RunDecoder <infoLength> <r-numerator> <r-denominator> <ebno> <numpackets>\n" );
     exit(-1);
   }
-  infoleng = atoi(argv[1]);
+  infoLeng = atoi(argv[1]);
   rnum = atoi(argv[2]);
   rdenom = atoi(argv[3]);
-  ebno = (float)atof(argv[4]);
-  sigFile = argv[5];
-  sprintf(mapFile, "./G_and_H_Matrices/Maps_%d%d_%d.bin", rnum, rdenom, infoleng);
-  printf("Using files:  %s and %s\n", mapFile, sigFile);
+  rNominal = float(rnum)/float(rdenom);
+  ebno = atof(argv[4]);
+  how_many = atoi(argv[5]);
+  sprintf(mapFile, "./G_and_H_Matrices/Maps_%d%d_%d.bin", rnum, rdenom, infoLeng);
+  sprintf(wROM_File, "./G_and_H_Matrices/W_ROW_ROM_%d%d_%d.bin", rnum, rdenom, infoLeng);
+
+
+  // Noise variance and log-likelihood ratio (LLR) scale factor. Because
+  // Ec = R*Eb is unity (i.e. we transmit +1's and -1's), then No = 1/(R*EbNo).
+  No = 1/(rNominal * pow(10,(ebno/10)));
+  sigma2 = No/2;
+  // When r is scaled by Lc it results in precisely scaled LLRs
+  lc = 4/No;
+
 
   src = fopen(mapFile, "r");
   if (src == NULL) {
@@ -66,65 +99,97 @@ int main (int argc, char **argv) {
   fread(mapRows2Cols, sizeof(unsigned int), numChecks* (maxBitsForCheck+1), src);
   fclose(src);
 
-  src = fopen(sigFile, "r");
+  src = fopen(wROM_File, "r");
   if (src == NULL) {
     errnum = errno;
     printf("Value of errno: %d\n", errnum);
     perror("Error printed by perror");
-    printf("Error opening file %s\n", sigFile);
+    printf("Error opening file %s\n", mapFile);
     return(EXIT_FAILURE);
   }
 
-  fread(& numSigs, sizeof(unsigned int), 1, src);
-  fread(& sigLength, sizeof(unsigned int), 1, src);
-  receivedSigs = (float *) malloc (sizeof(float)*numSigs*sigLength);
-  fread (receivedSigs, sizeof(float), numSigs*sigLength, src);
+  fread(& numRowsW, sizeof(unsigned int), 1, src);
+  fread(& numColsW, sizeof(unsigned int), 1, src);
+  fread(& shiftRegLength, sizeof(unsigned int), 1, src);
+  W_ROW_ROM = (unsigned int*) malloc(numRowsW * numColsW * sizeof( unsigned int));
+  fread(W_ROW_ROM, sizeof(unsigned int), numRowsW * numColsW, src);
+  numParityBits = numColsW;
   fclose(src);
+
 
   printf("parameters have been read.\n");
   printf("numBits = %i, numChecks = %i\n", numBits, numChecks);
-  printf("%i %i %i %i\n", maxChecksForBit, maxBitsForCheck, sigLength, numSigs);
+  printf("infoLeng = %i, numParityBits = %i (%i), numBits = %i\n",
+         infoLeng, numParityBits, infoLeng + numParityBits, numBits);
+  printf("%i %i\n", maxChecksForBit, maxBitsForCheck);
+  printf("ebn0 = %f, sigma = %f\n", ebno, sigma2);
+
   // ///////////////////////////////////////////
 
-  unsigned int decision[sigLength];
-  float estimates[sigLength];
-  unsigned int sigStartIndex;
+  unsigned int decision[numBits];
+  float estimates[numBits];
 
   unsigned int successes = 0;
   unsigned int iterationSum = 0;
-  unsigned int numreps = 1;
 
   // Allocate CUDA events that we'll use for timing
   cudaEvent_t start;
   HANDLE_ERROR(cudaEventCreate(&start));
   cudaEvent_t stop;
   HANDLE_ERROR(cudaEventCreate(&stop));
-  HANDLE_ERROR(cudaEventRecord(start, NULL));
+  float msecRecord = 0.0f;
+  float msecTotal = 0.0f;
 
   int itersHistory[HISTORY_LENGTH];
   int iters;
-  int how_many;
 
-  how_many = numSigs;
-  //  how_many = 1;
+  unsigned int* infoWord;
+  unsigned int* codeWord;
+  float s, noise;
+  float* receivedSig;
+
+  infoWord = (unsigned int *)malloc(infoLeng * sizeof(unsigned int));
+  codeWord = (unsigned int *)malloc((infoLeng+numParityBits) * sizeof(unsigned int));
+  receivedSig = (float *)malloc(numBits * sizeof(float));
 
   for (unsigned int i=0; i< how_many; i++) {
-    sigStartIndex = (i % numSigs) * sigLength;
-    iters = ldpcDecoder(& receivedSigs[sigStartIndex], numChecks, numBits,
+
+    for (unsigned int j=0; j < infoLeng; j++) {
+      infoWord[j] = (0.5 >= rDist(generator))? 1:0;
+    }
+    ldpcEncoder(infoWord, W_ROW_ROM, infoLeng, numRowsW, numColsW, shiftRegLength, codeWord);
+
+    // Modulate the codeWord, and add noise
+    for (unsigned int j=0; j < (infoLeng+numParityBits) ; j++) {
+      s     = 2*float(codeWord[j]) - 1;
+      // AWGN channel
+      noise = sqrt(sigma2) * normDist(generator);
+      // When r is scaled by Lc it results in precisely scaled LLRs
+      receivedSig[j]  = lc*(s + noise);
+    }
+
+    // The LDPC codes are punctured, so the r we feed to the decoder is
+    // longer than the r we got from the channel. The punctured positions are filled in as zeros
+    for (unsigned int j=(infoLeng+numParityBits); j<numBits; j++) receivedSig[j] = 0.0;
+
+    // Finally, ready to decode signal
+
+    HANDLE_ERROR(cudaEventRecord(start, NULL));
+    iters = ldpcDecoder(receivedSig, numChecks, numBits,
                         maxBitsForCheck, maxChecksForBit, mapRows2Cols, mapCols2Rows, MAXITERATIONS,
                         decision, estimates);
+    // Record the stop event
+    HANDLE_ERROR( cudaEventRecord(stop, NULL));
+    HANDLE_ERROR( cudaEventSynchronize(stop));
+    HANDLE_ERROR( cudaEventElapsedTime(&msecRecord, start, stop));
+    msecTotal += msecRecord;
+
     if (iters < MAXITERATIONS) {successes++;}
     iterationSum = iterationSum + iters;
     if ( i < HISTORY_LENGTH) { itersHistory[i] = iters;}
   }
 
-  // Record the stop event
-  HANDLE_ERROR( cudaEventRecord(stop, NULL));
-  // Wait for the stop event to complete
-  HANDLE_ERROR( cudaEventSynchronize(stop));
-  float msecTotal = 0.0f;
-  HANDLE_ERROR( cudaEventElapsedTime(&msecTotal, start, stop));
-  printf("%f msec to decode %i packets.\n", msecTotal, how_many, numreps);
+  printf("%f msec to decode %i packets.\n", msecTotal, how_many);
 
   printf(" %i Successes out of %i inputs.\n", successes, how_many);
   printf(" %i cumulative iterations, or about %.1f per packet.\n", iterationSum, iterationSum/(float)how_many);
