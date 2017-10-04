a1755867d08256429bcc5eab935141df41ef6096
komp <komp@ittc.ku.edu>
2017-10-04 16:30:11 -0500

    function declarations in an include file.

---
+++ LDPC.h
@@ -0,0 +1,52 @@
+
+__global__ void
+checkNodeProcessingOptimal (unsigned int numChecks, unsigned int maxBitsForCheck,
+                            float *lambdaByCheckIndex, float *eta);
+__global__ void
+checkNodeProcessingOptimalBlock (unsigned int numChecks, unsigned int maxBitsForCheck,
+                                 float *lambdaByCheckIndex, float *eta);
+
+__global__ void
+checkNodeProcessingMinSum (unsigned int numChecks, unsigned int maxBitsForCheck,
+                           float *lambdaByCheckIndex, float *eta);
+
+__global__ void
+checkNodeProcessingMinSumBlock (unsigned int numChecks, unsigned int maxBitsForCheck,
+                                float *lambdaByCheckIndex, float *eta);
+__global__ void
+checkNodeProcessingOptimalNaive (unsigned int numChecks, unsigned int maxBitsForCheck,
+                                 float *lambdaByCheckIndex, float *eta);
+__global__ void
+bitEstimates(float *rSig, float *etaByBitIndex, float *lambda,
+             unsigned int numBits, unsigned int maxChecksForBit);
+
+__global__ void
+transposeRC (unsigned int* map, float *checkRows, float *bitRows,
+             unsigned int numChecks, unsigned int maxBitsForCheck);
+
+__global__ void
+copyBitsToCheckmatrix (unsigned int* map, float *bitEstimates, float *checkRows,
+                       unsigned int *hd,
+                       unsigned int numBits, unsigned int maxChecksForBit);
+
+__global__ void
+calcParityBits (unsigned int* cHat, unsigned int *parityBits, unsigned int numChecks, unsigned int maxBitsForCheck);
+
+
+void ldpcEncoder (unsigned int *infoWord, unsigned int* W_ROW_ROM,
+                  unsigned int numMsgBits, unsigned int numRowsinRom, unsigned int numParBits,
+                   unsigned int shiftRegLength,
+                  unsigned int *codeWord);
+
+int ldpcDecoder (float *rSig, unsigned int numChecks, unsigned int numBits,
+                 unsigned int maxBitsForCheck, unsigned int maxChecksForBit,
+                 unsigned int *mapRows2Cols, unsigned int *mapCols2Rows,
+                 unsigned int maxIterations,
+                 unsigned int *decision,
+                 float *estimates);
+
+void initLdpcDecoder  (unsigned int numChecksI, unsigned int numBitsI,
+                       unsigned int maxBitsForCheckI, unsigned int maxChecksForBitI,
+                       unsigned int *mapRows2Cols, unsigned int *mapCols2Rows);
+
+int ldpcDecoderWithInit (float *rSig, unsigned int  maxIterations, unsigned int *decision, float *estimates);
