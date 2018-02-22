e12ae4cc8b2e354dedd121db27a6334cc154902c
komp <komp@ittc.ku.edu>
2018-02-22 10:05:09 -0600

    Correction.
    Original code had just thread 0 compute the product;
    then each thread divided thread this product by the value
    associated with the thread (removing itself from the product).
    
    This should work -- but apparently did not perform exactly as I expected.
    
    SINCE, all the threads are waiting on thread 0 to compute the product,
    on a GPU it makes more sense for each thread to compute the product
    not including itself.
    
    Clearer AND more correct code.

--- cnpOptimalBlock.cu
+++ cnpOptimalBlock.cu
@@ -20,6 +20,7 @@ checkNodeProcessingOptimalBlock (unsigned int numChecks, unsigned int maxBitsFor
   unsigned int m, n;
   unsigned int thisRowLength, currentIndex;
   bundleElt arg, value;
+  bundleElt myprod;
 
   m = blockIdx.x;
   n = threadIdx.x + 1;
@@ -37,19 +38,10 @@ checkNodeProcessingOptimalBlock (unsigned int numChecks, unsigned int maxBitsFor
       rowVals[n] = value;
       __syncthreads();
 
-      // Using JUST thread 0 to compute the product of all terms.
-      // Storing it in the shared location  rowVals[0]
-      if (threadIdx.x == 0) {
-        rowVals[0] = make_bundleElt(1.0);
-        for (unsigned int j=1; j<= thisRowLength; j++) rowVals[0] *= rowVals[j];
-      }
-      __syncthreads();
+      myprod = make_bundleElt(1.0);
+      for (unsigned int j=1; j<= thisRowLength; j++) if (j != n) myprod *= rowVals[j];
 
-      // value = -2 *atanhf(rowVals[0]/rowVals[n]);
-      arg = rowVals[0]/rowVals[n];
-      for (unsigned int slot=0; slot< SLOTS_PER_ELT; slot++) {
-        value.s[slot] = -2 * atanhf(arg.s[slot]);
-      }
+      for (unsigned int slot=0; slot< SLOTS_PER_ELT; slot++) value.s[slot] = -2 * atanhf(myprod.s[slot]);
       value = clamp(value, -MAX_ETA, MAX_ETA);
       eta[currentIndex] =  value;
       etaByBitIndex[ mapRows2Cols[currentIndex] ] = value;
